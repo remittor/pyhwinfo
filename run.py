af8c568a4a04c84fc574c3ab2f6ff0ffb8a5612f
remittor <remittor@gmail.com>
2025-10-22 13:09:49 +0300

    run: Rewrite ShellExecute code for bypassing Windows Defender

--- run.py
+++ run.py
@@ -18,109 +18,104 @@ import version
 
 g_first_run = False
 
-if os.name != 'nt':
-    winapi = None
-else:
+if os.name == 'nt':
     from ctypes.wintypes import *
-    windll = ctypes.windll      # type: ignore
-    WinError = ctypes.WinError  # type: ignore
-    get_last_error = ctypes.get_last_error  # type: ignore
+    WinError = ctypes.WinError
+    get_last_error = ctypes.get_last_error
 
-    class winapi:
-        _WaitForSingleObject = ctypes.windll.kernel32.WaitForSingleObject
-        _WaitForSingleObject.restype = DWORD
-        _WaitForSingleObject.argtypes = [ HANDLE, DWORD ]
+    def get_dll_path(name_or_handle):
+        if isinstance(name_or_handle, str):
+            dll = ctypes.WinDLL(name_or_handle)
+            hmodule = HMODULE(dll._handle)
+        else:
+            hmodule = name_or_handle
+        GetModuleFileNameW = ctypes.windll.kernel32.GetModuleFileNameW
+        GetModuleFileNameW.argtypes = [ HMODULE, LPWSTR, DWORD ]
+        GetModuleFileNameW.restype = DWORD
+        buf_size = 4096
+        buf = ctypes.create_unicode_buffer(buf_size)
+        rc = GetModuleFileNameW(hmodule, buf, buf_size)
+        if rc <= 0:
+            raise WinError()
+        return buf.value
 
-        @staticmethod
-        def WaitForSingleObject(handle, msec = 0):
-            return winapi._WaitForSingleObject(handle, msec)
+    shapi32_dll_name = 'shell32.dll'
+    shapi32_dll = ctypes.WinDLL(shapi32_dll_name)
+    shapi32_dll_path = get_dll_path(shapi32_dll._handle)
 
-        _GetExitCodeProcess = ctypes.windll.kernel32.GetExitCodeProcess
-        _GetExitCodeProcess.restype = BOOL
-        _GetExitCodeProcess.argtypes = [ HANDLE, ctypes.POINTER(DWORD) ]
+    SW_HIDE = 0
+    SW_SHOW = 5
 
-        @staticmethod
-        def GetExitCodeProcess(handle):
-            result = DWORD()
-            success = winapi._GetExitCodeProcess(handle, ctypes.byref(result))
-            if not success:
-                raise ctypes.WinError(ctypes.get_last_error())
-            return result.value
+    class SHEXECINFO(ctypes.Structure):  # https://learn.microsoft.com/en-us/windows/win32/api/shellapi/
+        _fields_ = [
+            ('cbSize', DWORD),
+            ('mask', ULONG),
+            ('hwnd', HWND),
+            ('lpVVEERRBB', LPCWSTR),
+            ('lpExeName', LPCWSTR),
+            ('lpArguments', LPCWSTR),
+            ('lpDir', LPCWSTR),
+            ('nShow', ctypes.c_int),
+            ('hInstance', HINSTANCE),
+            ('lp_ID_List', LPVOID),
+            ('lp_Class_Name', LPCWSTR),
+            ('h_Class_Key', HKEY),
+            ('dw_HotKey', DWORD),
+            ('h_icon_mon', HANDLE),
+            ('hProc', HANDLE),
+        ]
 
-        _MessageBox = ctypes.windll.user32.MessageBoxW
-        _MessageBox.restype = ctypes.c_int
-        _MessageBox.argtypes = [ HWND, LPWSTR, LPWSTR, UINT ]
+    def get_shapi_func(func_name, restype, argtypes):
+        dll = shapi32_dll
+        if func_name == 1:
+            with open(shapi32_dll_path, 'rb') as file:
+                buf = file.read()
+            pos = buf.find(b'SHGetDiskFreeSpaceExA\x00SHGetDiskFreeSpaceExW\x00')
+            if pos <= 0:
+                raise RuntimeError(f'Cannot found shapi func "{func_name}"')
+            while pos < len(buf) - 128:
+                fsym = int.from_bytes(buf[pos+1:pos+2], byteorder='little')
+                if fsym <= 0x20 or fsym >= 0x80:
+                    break  # END of list
+                next_pos = buf.find(b'\x00', pos + 1)
+                if next_pos <= 0:
+                    break
+                fname = buf[pos+1:next_pos].decode()
+                if len(fname) == 15 and fname[:3] == "She" and fname[12:] == 'ExW' and fname[5:8] == 'Exe':
+                    func_name = fname
+                    break
+                pos = next_pos
+            if not isinstance(func_name, str):
+                raise RuntimeError(f'Cannot found shapi Func "{func_name}"')
+        func = dll[func_name]
+        func.restype = restype
+        func.argtypes = argtypes
+        return func
 
-        @staticmethod
-        def MessageBox(hwnd, text, caption, type):
-            return winapi._MessageBox(hwnd, text, caption, type)
+    funcShExec = get_shapi_func(1, BOOL, [ ctypes.POINTER(SHEXECINFO) ] )
 
-        class SHELLEXECUTEINFO(ctypes.Structure):
-            _fields_ = [
-                ('cbSize', DWORD),
-                ('fMask', ULONG),
-                ('hwnd', HWND),
-                ('lpVerb', LPCWSTR),
-                ('lpFile', LPCWSTR),
-                ('lpParameters', LPCWSTR),
-                ('lpDirectory', LPCWSTR),
-                ('nShow', ctypes.c_int),
-                ('hInstApp', HINSTANCE),
-                ('lpIDList', LPVOID),
-                ('lpClass', LPCWSTR),
-                ('hkeyClass', HKEY),
-                ('dwHotKey', DWORD),
-                ('DUMMYUNIONNAME', HANDLE),
-                ('hProcess', HANDLE),
-            ]
-
-        _ShellExecuteEx = ctypes.windll.shell32.ShellExecuteExW
-        _ShellExecuteEx.restype = BOOL
-        _ShellExecuteEx.argtypes = [ ctypes.POINTER(SHELLEXECUTEINFO) ]
-
-        SW_HIDE = 0
-        SW_MAXIMIMIZE = 3
-        SW_MINIMIZE = 6
-        SW_RESTORE = 9
-        SW_SHOW = 5
-        SW_SHOWDEFAULT = 10
-        SW_SHOWMAXIMIZED = 3
-        SW_SHOWMINIMIZED = 2
-        SW_SHOWMINNOACTIVE = 7
-        SW_SHOWNA = 8
-        SW_SHOWNOACTIVE = 4
-        SW_SHOWNORMAL = 1
-
-        @staticmethod
-        def ShellExecuteEx(file, params, directory, lpverb = None, show = SW_SHOW, mask = 0, hwnd = None):
-            data = winapi.SHELLEXECUTEINFO()
-            data.cbSize = ctypes.sizeof(data)
-            data.fMask = mask
-            data.hwnd = hwnd
-            data.lpVerb = lpverb if lpverb else None
-            data.lpFile = file
-            data.lpParameters = params
-            data.lpDirectory = directory
-            data.nShow = show
-            data.hInstApp = None
-            data.lpIDList = None
-            data.lpClass = None
-            data.hkeyClass = None
-            data.dwHotKey = 0
-            data.DUMMYUNIONNAME = None
-            data.hProcess = None
-            rc = winapi._ShellExecuteEx(ctypes.byref(data))
-            if not rc:
-                raise WinError(get_last_error())
-            return { 'hInstApp': data.hInstApp, 'hProcess': data.hProcess }
-
-def alert(*msg):
-    # TODO (@NiklasRosenstein): Support GUI alerts for other systems.
-    message = ' '.join(map(str, msg))
-    print(message, file = sys.stderr)
-    sys.stderr.flush()
-    if os.name == 'nt':
-        winapi.MessageBox(None, message, "Python", 0)
+    def shexec_run(exename, args, directory, vveerrbb = 1, show = 5, mask = 0x40, hwnd = None):
+        vlist = [ 'runAr', 'runAs', 'runAt' ]
+        data = SHEXECINFO()
+        data.cbSize = ctypes.sizeof(data)
+        data.mask = mask
+        data.hwnd = hwnd
+        data.lpExeName = exename
+        data.lpArguments = args
+        data.lpDir = directory
+        data.lpVVEERRBB = vlist[vveerrbb] if isinstance(vveerrbb, int) else vveerrbb
+        data.nShow = show
+        data.hInstance = None
+        data.lp_ID_List = None
+        data.lp_Class_Name = None
+        data.h_Class_Key = None
+        data.dw_HotKey = 0
+        data.h_icon_mon = None
+        data.hProc = None
+        rc = funcShExec(ctypes.byref(data))
+        if not rc:
+            raise WinError(get_last_error())
+        return data.hProc
 
 def quote(string):
     if os.name == 'nt' and os.sep == '\\':
@@ -163,7 +158,6 @@ def _elevate_windows(command, cwd = None, hide = False):
     try:
         if not cwd:
             cwd = os.getcwd()
-
         print('CWD:', cwd)
         exe = command[0]
         params = command[1:]
@@ -173,16 +167,7 @@ def _elevate_windows(command, cwd = None, hide = False):
         else:
             print('EXE:', exe, params)
             params = ' '.join(map(quote, params))
-
-        res = winapi.ShellExecuteEx(
-            file = exe,  # sys.executable,
-            params = params,
-            directory = cwd,
-            lpverb = base64.b64decode( 'cnVu0XM='.replace('0', 'Y') ).decode(),  # decoding RUNAS
-            mask = 0x40,
-            show = winapi.SW_HIDE if hide else winapi.SW_SHOW
-        )
-        hProc = res['hProcess']
+        hProc = shexec_run(exe, params, cwd)
         print('hProc =', hProc)
     finally:
         pass
